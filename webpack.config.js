f3440979898130e4168a58f8079141cfb2b2411e
HigorVinicius <higor.tecinfor@gmail.com>
2018-11-01 17:25:28 -0300

    fix webpack confing

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  output: {
+    library: 'SpotifyWrapper',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    filename: 'spotify-wrapper.js',
+    umdNamedDefine: true
+  }
+};
